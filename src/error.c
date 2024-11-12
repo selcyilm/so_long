6b33314bea5e37677b2f39438ff46ca546b933ec
Selcuk <selcukemnn@gmail.com>
2024-11-12 19:54:33 +0100

    refactor: change format of error messages

--- src/error.c
+++ src/error.c
@@ -3,14 +3,14 @@
 void	error_msg(int num)
 {
 	if (num == 1)
-		ft_printf("Error: Missing arguments!\n");
+		ft_printf("Error\nMissing arguments!\n");
 	else if (num == 2)
-		ft_printf("Error: Invalid File name!\n");
+		ft_printf("Error\nInvalid File name!\n");
 	else if (num == 3)
-		ft_printf("Error: Opening file!\n");
+		ft_printf("Error\nOpening file!\n");
 	else if (num == 4)
-		ft_printf("Error: Malloc failed!\n");
+		ft_printf("Error\nMalloc failed!\n");
 	else if (num == 5)
-		ft_printf("Error: Invalid Map!\n");
+		ft_printf("Error\nInvalid Map!\n");
 	exit(1);
 }
