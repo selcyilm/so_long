a6cb6d672fb38d7a8a04aa321b9cb3091e88d294
Selcuk Emin Yilmaz <selcyilm@f1r1s9.codam.nl>
2024-11-15 15:31:38 +0100

    chore: clean some unused include libs

--- include/so_long_bonus.h
+++ include/so_long_bonus.h
@@ -12,13 +12,11 @@
 
 #ifndef SO_LONG_BONUS_H
 # define SO_LONG_BONUS_H
-# include <stdio.h>
 # include "../lib/libft/libft.h"
 # include "../lib/libft/ft_printf/ft_printf.h"
 # include "../lib/libft/get_next_line/get_next_line.h"
 # include "../lib/MLX42/include/MLX42/MLX42.h"
 # include <fcntl.h>
-# include <unistd.h>
 
 # define PIXEL 96
 
